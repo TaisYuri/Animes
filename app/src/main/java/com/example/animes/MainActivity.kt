4f6732f29760d57aac2078c8034efe74b9ced7bc
taisyuri <taisyuri@hotmail.com>
2024-09-30 13:34:54 -0300

    Created global auth

--- app/src/main/java/com/example/animes/MainActivity.kt
+++ app/src/main/java/com/example/animes/MainActivity.kt
@@ -5,12 +5,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navOptions
 import com.example.animes.domain.model.UserPreferences
+import com.example.animes.presentation.AppState
 import com.example.animes.presentation.UserViewModel
 import com.example.animes.presentation.navigation.AnimeNavHost
+import com.example.animes.presentation.navigation.InitialRoute
+import com.example.animes.presentation.navigation.LoginRoute
 import com.example.animes.ui.theme.AnimeSTheme
 import org.koin.androidx.compose.koinViewModel
 
@@ -22,11 +27,28 @@ class MainActivity : ComponentActivity() {
         setContent {
             val userViewModel = koinViewModel<UserViewModel>()
             val isDarkMode by userViewModel.isDarkMode.collectAsState(initial = false)
+            val authState by userViewModel.state.collectAsState(initial = AppState())
+
+
+
             AnimeSTheme(
                 darkTheme = isDarkMode
             ) {
                 val navController = rememberNavController()
 
+                LaunchedEffect(authState) {
+                    if(authState.isInitialLoading){
+                        return@LaunchedEffect
+                    }
+                    val cleanBackStackNavOptions = navOptions {
+                        popUpTo(navController.graph.id) {
+                            inclusive = true
+                        }
+                    }
+                    authState.user?.let {
+                        navController.navigate(route = InitialRoute,cleanBackStackNavOptions)
+                    }?: navController.navigate(route = LoginRoute,cleanBackStackNavOptions)
+                }
                 Surface(
                     color = MaterialTheme.colorScheme.background
                 ) {
